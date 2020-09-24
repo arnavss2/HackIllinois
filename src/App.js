5cbfec256885dab3b37c5517e93afea79128725c
arniboy4 <68439835+arniboy4@users.noreply.github.com>
2020-09-25 04:39:38 +0530

    Initial Changes

--- src/App.js
+++ src/App.js
@@ -122,6 +122,44 @@ function App() {
         >
           Aug 15 Sat
         </Button>
+        <div className="events">
+          {events &&
+            events.map((event, index) => {
+              const date = moment
+                .unix(event.startTime)
+                .tz("America/Chicago")
+                .format();
+              const day = new Date(date).toDateString();
+              const from = moment
+                .unix(event.startTime)
+                .tz("America/Chicago")
+                .format()
+                .substring(11, 16);
+              const to = moment
+                .unix(event.endTime)
+                .tz("America/Chicago")
+                .format()
+                .substring(11, 16);
+              const description = event.description;
+              return (
+                <div className="event" key={index}>
+                  <h2>{event.name}</h2>
+                  <div className="details">
+                    <p>{day}</p>
+                    {description === " " ? null : <p>{description}</p>}
+                    {from !== to ? (
+                      <p>
+                        {from}-{to}
+                      </p>
+                    ) : (
+                      <p>{from}</p>
+                    )}
+                    <p>Event Type: {event.eventType}</p>
+                  </div>
+                </div>
+              );
+            })}
+        </div>
       </header>
     </div>
   );
--- src/components/Display.jsx
+++ src/components/Display.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import "./display.css";
 import moment from "moment";
 import "moment-timezone";
